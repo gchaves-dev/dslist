8229a2b1d21a21200a50abbcb0cad154b6f49ece
GustavoDev <gchaves427@gmail.com>
2025-06-25 23:35:31 -0300

    Project created

---
+++ src/test/java/com/listofGames/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listofGames.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
